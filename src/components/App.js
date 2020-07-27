e948054d8e7d2c0d9038de383929c233e7dab618
Gubics Andrea <gubics.andrea@gmail.com>
2020-07-27 07:27:33 +0200

    Setup routes to home page and login page

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,37 @@
 import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import LoadingBar from "react-redux-loading";
+import LoginPage from "./LoginPage";
+import HomePage from "./HomePage";
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
+    const { loading } = this.props;
     return (
-      <Fragment>
-        <LoadingBar />
-        <div>App</div>
-      </Fragment>
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          {loading === true ? null : (
+            <div>
+              <Route path="/" exact component={HomePage} />
+              <Route path="/login" component={LoginPage} />
+            </div>
+          )}
+        </Fragment>
+      </Router>
     );
   }
 }
 
-export default connect()(App);
+function mapStateToProps({ users }) {
+  return {
+    loading: users === []
+  };
+}
+
+export default connect(mapStateToProps)(App);
