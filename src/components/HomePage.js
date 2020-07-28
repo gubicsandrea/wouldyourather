b6d1d3f4bd6036aa47e5237fb7189e430b1eccb1
Gubics Andrea <gubics.andrea@gmail.com>
2020-07-28 08:13:04 +0200

    Homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,30 +3,73 @@ import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 
 class HomePage extends Component {
+  state = {
+    answeredVisible: false,
+    buttonText: "View Answered Question"
+  };
+
+  handleButtonClick = () => {
+    this.setState(prevState => ({
+      answeredVisible: !prevState.answeredVisible,
+      buttonText: prevState.answeredVisible
+        ? "View Answered Questions"
+        : "View Unanswered Questions"
+    }));
+  };
+
   render() {
-    const { authedUser, questions } = this.props;
-    console.log(authedUser);
-    console.log(questions);
+    const {
+      authedUser,
+      answeredQuestionIds,
+      unansweredQuestionIds
+    } = this.props;
+
     if (authedUser === null) {
       return <Redirect to="/login" />;
     }
-    return <div>Home page</div>;
+
+    const questionIds = this.state.answeredVisible
+      ? answeredQuestionIds
+      : unansweredQuestionIds;
+
+    return (
+      <div>
+        <h1>Home page</h1>
+        <button onClick={this.handleButtonClick}>
+          {this.state.buttonText}
+        </button>
+        <ul>
+          {questionIds.map(id => (
+            <li key={id}>{id}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
 function mapStateToProps({ authedUser, questions, users }) {
-  const allQuestions =
+  const answeredQuestionIds =
     authedUser !== null
-      ? Object.keys(questions).map(questionId => ({
-          ...questions[questionId],
-          answer: Object.keys(users[authedUser].answers).includes(questionId)
-            ? users[authedUser].answers[questionId]
-            : null
-        }))
+      ? Object.keys(questions).filter(
+          id =>
+            questions[id].optionOne.votes.includes(authedUser) ||
+            questions[id].optionTwo.votes.includes(authedUser)
+        )
+      : [];
+  const unansweredQuestionIds =
+    authedUser !== null
+      ? Object.keys(questions).filter(
+          id =>
+            !questions[id].optionOne.votes.includes(authedUser) &&
+            !questions[id].optionTwo.votes.includes(authedUser)
+        )
       : [];
   return {
     authedUser,
-    questions: allQuestions
+    answeredQuestionIds,
+    unansweredQuestionIds,
+    users
   };
 }
 
