510a6dd3ddee74e4686f2a801e1326364e72ab0b
Gubics Andrea <gubics.andrea@gmail.com>
2020-07-29 07:15:39 +0200

    Add logout feature

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAuthedUser } from "../actions/authedUser";
 
-export default function Nav() {
-  return (
-    <div>
-      <Link to="/">Home page</Link>
-      <Link to="/add">Ask a question</Link>
-      <Link to="/leaderboard">Leaderboard</Link>
-    </div>
-  );
+class Nav extends Component {
+  handleLogout = () => {
+    const { dispatch } = this.props;
+    dispatch(setAuthedUser(null));
+  };
+
+  render() {
+    const { authedUser } = this.props;
+    return (
+      <div>
+        <Link to="/">Home page</Link>
+        <Link to="/add">Ask a question</Link>
+        <Link to="/leaderboard">Leaderboard</Link>
+        {authedUser && <span onClick={this.handleLogout}>Logout</span>}
+      </div>
+    );
+  }
 }
+
+function mapStateToProps({ authedUser }) {
+  return {
+    authedUser
+  };
+}
+
+export default connect(mapStateToProps)(Nav);
