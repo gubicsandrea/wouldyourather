90ba638849f1483a34f73ecbaeea8caa8caf52c2
Gubics Andrea <gubics.andrea@gmail.com>
2020-08-02 16:45:08 +0200

    Make new question card narrower

--- src/components/NewQuestionPage.js
+++ src/components/NewQuestionPage.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { handleSaveNewQuestion } from "../actions/shared";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -45,39 +47,43 @@ class NewQuestionPage extends Component {
       return <Redirect to="/login" />;
     }
     return (
-      <Card>
-        <Card.Header>Create New Question</Card.Header>
-        <Card.Body>
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Group>
-              <Form.Label>Option One Text</Form.Label>
-              <Form.Control
-                type="text"
-                value={optionOne}
-                onChange={e => this.handleTextOneChange(e)}
-                placeholder="Enter option one text"
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Option Two Text</Form.Label>
-              <Form.Control
-                type="text"
-                value={optionTwo}
-                onChange={e => this.handleTextTwoChange(e)}
-                placeholder="Enter option two text"
-              />
-            </Form.Group>
-            <Button
-              variant="outline-success"
-              type="submit"
-              disabled={optionOne === "" || optionTwo === ""}
-              block
-            >
-              Add
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
+      <Row className="justify-content-md-center">
+        <Col md={8} lg={6}>
+          <Card>
+            <Card.Header>Create New Question</Card.Header>
+            <Card.Body>
+              <Form onSubmit={this.handleSubmit}>
+                <Form.Group>
+                  <Form.Label>Option One Text</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={optionOne}
+                    onChange={e => this.handleTextOneChange(e)}
+                    placeholder="Enter option one text"
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Option Two Text</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={optionTwo}
+                    onChange={e => this.handleTextTwoChange(e)}
+                    placeholder="Enter option two text"
+                  />
+                </Form.Group>
+                <Button
+                  variant="outline-success"
+                  type="submit"
+                  disabled={optionOne === "" || optionTwo === ""}
+                  block
+                >
+                  Add
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     );
   }
 }
